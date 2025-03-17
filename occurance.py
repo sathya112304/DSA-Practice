95a5bb91cbf3022e85e134ba7576b74eb684e670
Sathyasai Darla <sathyasaidarla11@gmail.com>
2025-03-17 20:58:31 +0530

    Initial commit

--- occurance.py
+++ occurance.py
@@ -1,3 +1,4 @@
+#Brute force approach
 def occurance(target):
     first,last=-1,-1
     for i in range(len(arr)):
@@ -13,3 +14,40 @@ def occurance(target):
 arr=[1,4,3,5,6,7,8,3]
 target=3
 print(set(occurance(target)))
+
+#Optimal approach
+def occurance(arr,target):
+    arr.sort()
+    def lower_bound(arr,target):
+        low,high=0,len(arr)-1
+        ans= len(arr)
+        while low<=high:
+            mid=(low+high)//2 
+            if arr[mid]>=target:
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ans
+    def upper_bound(arr,target):
+        low,high=0,len(arr)-1
+        ans=len(arr)
+        while low<=high:
+            mid=(low+high)//2
+            if arr[mid]>target:
+                ans=mid
+                high=mid-1
+            low=mid+1
+        return ans
+    
+    lb=lower_bound(arr,target)
+    ub=upper_bound(arr,target)
+    if lb==len(arr) or arr[lb]!=target:
+        return (-1,-1)
+    return (lb,ub-1)
+
+
+arr=[1,4,3,5,6,7,8,3] 
+target=3
+print(set(occurance(arr,target)))
+
